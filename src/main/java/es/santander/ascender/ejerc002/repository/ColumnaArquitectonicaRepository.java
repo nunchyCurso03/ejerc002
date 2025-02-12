859c705b8ca6d417cbcce4f8480483f7e0a636d3
nunchyCurso03 <you@example.com>
2025-02-12 13:57:29 +0100

    Utilizamos JpaRepository. Test

---
+++ src/main/java/es/santander/ascender/ejerc002/repository/ColumnaArquitectonicaRepository.java
@@ -0,0 +1,9 @@
+package es.santander.ascender.ejerc002.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import es.santander.ascender.ejerc002.model.ColumnaArquitectonica;
+
+public interface ColumnaArquitectonicaRepository extends JpaRepository<ColumnaArquitectonica, Long> {
+
+}
